08c70a3a33b181d3057848c10347d3edc3735f6a
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2023-10-29 16:01:51 +0300

    5-1

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import { ProductList } from "../components/ProductList";
+import { getProducts } from "../fakeAPI";
+
+export default function Products() {
+  const products = getProducts();
+  return (
+    <main>
+      <ProductList products={products} />
+    </main>
+  );
+};
