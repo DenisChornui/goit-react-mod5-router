62be38a5d42189cfc17e5bfed27466fdf9a58ab4
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2023-10-30 11:21:28 +0000

    Deploying to gh-pages from @ DenisChornui/goit-react-mod5-router@2c387c550b4764aa65be857c78e428c0bd9a4bcd 🚀

---
+++ static/js/719.61a15b89.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[719],{719:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});var n=t(184);function u(){return(0,n.jsx)("div",{children:"Error"})}}}]);
+//# sourceMappingURL=719.61a15b89.chunk.js.map
